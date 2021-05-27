91985782304437fa7313e077603ad8d3560c3f04
Vadim Turok <turokvadim2510@gmail.com>
2021-05-27 13:50:53 +0300

    changes in index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import {dfs1} from "./tasks/task1.js";
-import {dfs} from "./tasks/task2.js";
-import {graph} from "./graphs.js";
-import {graph2} from "./graphs.js";
-import {findShortestPath} from "./tasks/task3.js";
-import {str} from "./tasks/task2.js";
-import {insertionSort} from "./tasks/task4.js";
-import {city} from "./graphs.js";
-import {graph3} from "./graphs.js";
+import {dfs1} from "./tasks/task1.js"
+import {dfs} from "./tasks/task2.js"
+import {graph} from "./graphs.js"
+import {graph2} from "./graphs.js"
+import {findShortestPath} from "./tasks/task3.js"
+import {str} from "./tasks/task2.js"
+import {insertionSort} from "./tasks/task4.js"
+import {city} from "./graphs.js"
+import {graph3} from "./graphs.js"
 
 let path = ''
 let path2 = ''
 let shortestPath = findShortestPath(graph2, 'uzhgorod', 'dovhe')
-shortestPath.path.map(city => {path += city; path+= ' --> '})
+shortestPath.path.map(city => path += city + ' --> ')
 let shortestPath2 = findShortestPath(graph3, 'uzhgorod', 'dovhe')
-shortestPath2.path.map(city => {path2 += city; path2+= ' --> '})
+shortestPath2.path.map(city => path2 += city + ' --> ')
 dfs1(graph, 'uzhgorod', 'dovhe')
 dfs(graph, 'uzhgorod', 'dovhe')
-console.log('Path: ', str, '\nDistance: ', shortestPath.distance, 'km', '\nShortest path by km: ', path, '\nShortest path by priority: ', path2)
+console.log(str, '\n', shortestPath.distance, 'km\n', path, '\n', path2)
 let res = insertionSort(city, 1)
-for(let i of res) console.log(res.indexOf(i)+1, ': ', i.name)
+for(let i of res) console.log(i.name)
 console.log('')
 res = insertionSort(city, 2)
-for(let i of res) console.log(res.indexOf(i)+1, ': ', i.name, ' -- ', i.population)
+for(let i of res) console.log(i.name, ' -- ', i.population)
--- tasks/task1.js
+++ tasks/task1.js
@@ -1,9 +1,7 @@
 const visited = {}
 export function dfs1(adj, v, t) {
     console.log(v)
-    if(visited[v]){
-        return false
-    }
+    if(visited[v]) return false
     visited[v] = true
     for(let neighbor of adj[v]) {
         if(!visited[neighbor]) {
